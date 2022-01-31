e1179e4d18378307b65bdea71ca46b3797d39491
abhinayy0 <abhinayy0@gmail.com>
2022-01-31 12:06:45 +0530

    Added warnings for invalid urls Only one qr code will generate for unique urls

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    SECRET_KEY = "Notsecret"
     QR_FOLDER = os.path.join(basedir, "static")
+    DEBUG= True
